0516407561ce6a098bea49d3370471ff667ba3f4
Mihon <75487015+MikhailKolomoitsev@users.noreply.github.com>
2022-06-02 18:01:43 +0000

    8-45

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,20 +1,33 @@
-import {UserAction, UserActionTypes, UserState} from "../../types/user";
+const FETCH_USERS = 'FETCH_USERS'
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'
+const FETCH_USERS_ERROR = 'FETCH_USERS_ERROR'
 
-const initialState: UserState = {
-    users: [],
-    loading: false,
-    error: null
+interface IUserState {
+  users: any[]
+  loading: boolean
+  error: null | boolean
 }
 
-export const userReducer = (state = initialState, action: UserAction): UserState => {
-    switch (action.type) {
-        case UserActionTypes.FETCH_USERS:
-            return {loading: true, error: null, users: []}
-        case UserActionTypes.FETCH_USERS_SUCCESS:
-            return {loading: false, error: null, users: action.payload}
-        case UserActionTypes.FETCH_USERS_ERROR:
-            return {loading: false, error: action.payload, users: []}
-        default:
-            return state
-    }
+interface IUserAction{
+    type: string,
+    payload?:any
+}
+
+const initialState: IUserState = {
+  users: [],
+  loading: false,
+  error: null,
+}
+
+export const userReducer = (state = initialState, action:IUserAction): IUserState => {
+  switch (action.type) {
+    case FETCH_USERS:
+      return { loading: true, error: null, users: [] }
+    case FETCH_USERS_SUCCESS:
+      return { loading: false, error: null, users: action.payload}
+    case FETCH_USERS_ERROR:
+      return { loading: false, error: action.payload, users: [] }
+    default:
+      return state
+  }
 }
